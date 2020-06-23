4e7efca7bfc0005398ffb5f3e231d6ae8ba10a40
Mubashir <mavayani2@gmail.com>
2020-06-23 22:04:01 +0500

    Complete

--- app.js
+++ app.js
@@ -56,9 +56,128 @@
 
 // Chapter 21-25 Task #8
 
-var message = "Ali and Sami are best friends. They play cricket and football together.";
-var newMessage;
-for(var i=0;i<message.length;i++){
-    newMessage = message.replace("and","&");
-};
-alert(newMessage);
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var newMessage;
+// for(var i=0;i<message.length;i++){
+//     if(message.slice(i,i+3) === "and"){
+//         message = message.slice(0,i) + "&" + message.slice(i + 3);
+//     };
+// };
+// alert(message);
+
+
+// Chapter 21-25 Task #9
+
+// var stringNum = "472";
+// document.write("Value: " + stringNum + "<br>");
+// document.write("Type: " + typeof stringNum + "<br>");
+// var stringToNum = +stringNum;
+// document.write("Value: " + stringToNum + "<br>");
+// document.write("Type: " + typeof stringToNum + "<br>");
+
+
+// Chapter 21-25 Task #10
+
+// var str3 = prompt("Enter any String");
+// document.write("User input: "+str3+"<br>");
+// var str3 = str3.toUpperCase();
+// document.write("Upper case: "+str3+"<br>");
+
+
+// Chapter 21-25 Task #11
+
+// var str4 = prompt("Enter the Title");
+// var FCStr4 = str4.slice(0,1);
+// FCStr4 = FCStr4.toUpperCase();
+// var OCStr4 = str4.slice(1);
+// OCStr4 = OCStr4.toLowerCase();
+// var titleStr4 = FCStr4.concat(OCStr4)
+// document.write("User input: "+str4+"<br>");
+// document.write("Title case: "+titleStr4);
+
+
+// Chapter 21-25 Task #12
+
+// var num = 35.36;
+// var numString = num.toString();
+// numString = numString.replace(".","");
+// document.write("Number: "+num+"<br>");
+// document.write("Result: "+numString+"<br>");
+
+
+// Chapter 21-25 Task #13
+
+// var username = prompt("Enter the username");
+// for(var i=0;i<username.length;i++){
+//     if(username[i] == "@" || username[i] == "." || username[i] == "," || username[i] == "!"){
+//         alert("Please enter a valid username");
+//         break;
+//     };
+// };
+
+
+// Chapter 21-25 Task #14
+
+// A = ["cake", "apple pie", "cookie", "chips", "patties"];
+// var search = prompt("Welcome to ABC Bakery, what do you want to order sir/ma'am?");
+// search = search.toLowerCase();
+// for(var i=0;i<A.length;i++){
+//     if(A[i] === search){
+//         document.write(search + " is <b>available</b> at index " + i + " in our bakery <br>");
+//         break;
+//     } else if(i<A.length){
+//         if(A[i] === search){
+//             document.write(search + " is <b>available</b> at index " + i + " in our bakery <br>");
+//             break;
+//         }
+//     } else document.write("We are sorry. " + search + " is <b>not available</b> in our bakery <br>")
+    
+// };
+
+
+// Chapter 21-25 Task #15
+
+// var password = prompt("Enter your password");
+// var passwordFC = password.slice(0,1);
+// passwordFC = passwordFC.charCodeAt();
+// if (passwordFC >= 48 || passwordFC <= 57){
+//     alert("Password can not begin with a number");
+//     password = prompt("Enter your password");
+// } else if (password.length <= 5){
+//     alert("Password should have at least 6 characters");
+//     password = prompt("Enter your password");
+// };
+// document.write("Your Password: " + password);
+
+
+// Chapter 21-25 Task #16
+
+// var university = "University of Karachi";
+// var universityLength = university.length;
+// var universityArray = university.split("",universityLength);
+// for(var i=0;i<universityArray.length;i++){
+//     document.write(universityArray[i] + "<br>");
+// };
+
+
+// Chapter 21-25 Task #17
+
+// var str5 = prompt("Enter any string");
+// document.write("User input: " + str5 + "<br>");
+// str5Length = str5.length;
+// document.write("Last character of input: " + str5[str5Length-1]);
+
+
+// Chapter 21-25 Task #18
+
+// var text = "The quick brown fox jumps over the lazy dog";
+// var count=0;
+// document.write("Text: " + text + "<br>");
+// for(var i=0;i<text.length;i++){
+//     if(text.slice(i,i+3) == "the" || text.slice(i,i+3) == "The"){
+//         count++;
+//     };
+// };
+// document.write("There are " + count + " occurrence(s) of word 'The'");
+
+
